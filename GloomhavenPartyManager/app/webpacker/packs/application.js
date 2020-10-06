dfbdb677132f218151b498d4bf0119386ea8cb0b
Meg Wenzinger <forlornlawngnome@gmail.com>
2020-10-06 14:42:40 -0700

    add fontawesome

--- GloomhavenPartyManager/app/webpacker/packs/application.js
+++ GloomhavenPartyManager/app/webpacker/packs/application.js
@@ -15,9 +15,18 @@ $(document).on('turbolinks:load', function() {
   $(document).foundation()
 });
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, faTwitter)
+// Kicks off the process of finding <i> tags and replacing with <svg>
+dom.watch()
