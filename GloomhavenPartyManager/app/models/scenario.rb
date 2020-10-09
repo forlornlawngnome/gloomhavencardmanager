9be788ba3689d1905317c05481d0d00443a208f0
Meg Wenzinger <forlornlawngnome@gmail.com>
2020-10-08 18:43:24 -0700

    add scenario picker

--- GloomhavenPartyManager/app/models/scenario.rb
+++ GloomhavenPartyManager/app/models/scenario.rb
@@ -1,9 +1,12 @@
 class Scenario < ApplicationRecord
+	acts_as_tenant(:party)
   belongs_to :party
 
   has_many :character_scenarios
   has_many :characters, through: :character_scenarios
   has_many :rounds
+
+  scope :active, -> { where(active: true) }
 
   def display
   	return "#{self.number} - #{self.name}"
